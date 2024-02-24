3e97c782696d4388c83fe4edecb9dc5622bd5b2a
urosjarc <jar.fmf@gmail.com>
2024-02-24 15:36:03 +0100

    Finishing with sqlite custom queries.

--- src/demo/kotlin/002_sqlite_custom_queries.kt
+++ src/demo/kotlin/002_sqlite_custom_queries.kt
@@ -1,11 +1,10 @@
 import com.urosjarc.dbmessiah.data.QueryBuilder
-import com.urosjarc.dbmessiah.domain.Page
 import com.urosjarc.dbmessiah.domain.Table
 import com.urosjarc.dbmessiah.impl.sqlite.SqliteSerializer
 import com.urosjarc.dbmessiah.impl.sqlite.SqliteService
 import com.urosjarc.dbmessiah.serializers.AllTS
-import java.awt.event.InputEvent
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 /**
  * 1. Reuse sqlite and postgresql service defined in 000_basic_sqlite and 001_basic_postgresql
@@ -21,6 +20,15 @@ data class Child2(
     var value: String
 )
 
+data class Output2(
+    val child_value: String,
+    val parent_value: String
+)
+
+data class Input2(
+    val parent_pk: Int
+)
+
 /**
  * 2. Create database serializer and explain database structure...
  */
@@ -33,7 +41,9 @@ val sqliteSerializer1 = SqliteSerializer(
             )
         ),
     ),
-    globalSerializers = AllTS.basic
+    globalSerializers = AllTS.basic,
+    globalOutputs = listOf(Output2::class), // Note if you use custom objects as input or output you have to register them to global inputs or outputs.
+    globalInputs = listOf(Input2::class),   // This is because library uses reflection at initilization to scan objects of their properties, constructors etc...
 )
 
 val sqliteService1 = SqliteService(config = sqliteConfig, ser = sqliteSerializer1)
@@ -55,13 +65,16 @@ fun main_002() {
          *
          * > https://www.jetbrains.com/help/idea/using-language-injections.html
          */
-        it.run.query { "INSERT INTO Parent2 (pk, value) VALUES (123, 'asdf')" }
+        it.run.query { "INSERT INTO Parent2 (pk, value) VALUES (123, 'parent_asdf')" }
+        it.run.query { "INSERT INTO Child2 (pk, parent_pk, value) VALUES (321, 123, 'child_asdf')" }
 
         /**
          * 5. Write custom query with output
          */
         val output0 = it.run.query(output = Parent2::class) { "SELECT * FROM Parent2 WHERE pk = 123" }
+        val output1 = it.run.query(output = Child2::class) { "SELECT * FROM Child2" }
         assertEquals(output0[0].pk, 123)
+        assertEquals(output1.size, 1)
 
         /**
          * 6. Write SQL injection safe custom query with output and input
@@ -69,9 +82,9 @@ fun main_002() {
          * For input objects you can use any table registered in serializer, if you use custom objects (not tables)
          * you will have to defined them to globalInputs in the serializer constructor to ensure type safety.
          */
-        val output1 = it.run.query(
+        val output2 = it.run.query(
             output = Parent2::class,
-            input = Child2(value = "asdf") // For our input we can also use the same table but lets use different so that it's not confusing...
+            input = Child2(value = "parent_asdf") // For our input we can also use the same table but lets use different so that it's not confusing...
         ) { q: QueryBuilder<Child2> ->     // If you are using input you will be provided with query builder to help you ensure type safety and prevent SQL injections...
             """SELECT * FROM Parent2 WHERE value = ${q.get(Child2::value)}
                 
@@ -80,7 +93,32 @@ fun main_002() {
             -- q.get() will also register Kproperty1<INPUT, *> and find this property in input object to supply JDBC appropriate value to be replaced instead of '?' character.
             """
         }
-        assertEquals(output1[0].pk, 123)
+        assertEquals(output2[0].pk, 123)
+
+        /**
+         * 7. Write custom query with custom output and input
+         *
+         * Note that Sqlite driver only supports one output per database call thats why you can have only one output!
+         * Other databases can support many outputs per output since underling driver supports it...
+         * > https://github.com/xerial/sqlite-jdbc/issues/1062
+         *
+         * Those who use JetBrains please dont forget to inject sql string with reference so that you can directly test sql string on your database.
+         */
+        val output3 = it.run.query(
+            output = Output2::class,
+            input = Input2(parent_pk = 123)
+        ) {
+            """
+            SELECT
+                c.value AS ${Output2::child_value.name}, 
+                p.value AS ${Output2::parent_value.name} 
+            FROM Child2 c
+                JOIN Parent2 p ON c.parent_pk = p.pk
+            WHERE p.pk = ${it.get(Input2::parent_pk)}
+            """
+        }
+
+        assertTrue(output3.isNotEmpty())
 
     }
 }
